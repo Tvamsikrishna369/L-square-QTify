acb70636d04e4c6aa89afd1db1e23727364e4a86
Tvamsikrishna369 <nanivamsi369@gmail.com>
2024-06-26 00:27:17 +0530

    Hero-1

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,13 @@
 // import logo from './assets/logo'
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar"
+// import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Hero />
     </div>
   );
 }
